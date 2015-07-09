79c7b04f6b4645a40ea5de254718a3063a641740
Frazer Barnes <frazer.jsb@gmail.com>
2015-07-09 15:55:45 +0100

    API Helper classed added

--- papirus/__init__.py
+++ papirus/__init__.py
@@ -1,1 +1,3 @@
 from epd import EPD as Papirus
+from text import PapirusText
+from image import PapirusImage
---
+++ papirus/image.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+from PIL import Image
+from PIL import ImageDraw
+from papirus import Papirus
+import random
+
+class PapirusImage():
+    def __init__():
+        print("done")
