d334899c34a09001a78627c705d8e86284324ce3
Garage Tech <francesco.vannini@happylot.net>
2016-04-02 14:31:26 +0100

    Update text.py
    Fixed inheritance

--- papirus/text.py
+++ papirus/text.py
@@ -3,18 +3,22 @@ import sys
 
 from PIL import Image
 from PIL import ImageDraw
+from PIL import ImageFont
 from papirus import Papirus
 import random
 
+WHITE = 1
+BLACK = 0
+
 class PapirusText():
 
-    def __init__():
-        self.papirus = Papirus()
+    def __init__(self):
+        Papirus.__init__(self)
 
     def write(self, text, size = 20):
 
         # initially set all white background
-        image = Image.new('1', papirus.size, WHITE)
+        image = Image.new('1', self.size, WHITE)
 
         # prepare for drawing
         draw = ImageDraw.Draw(image)
@@ -22,7 +26,7 @@ class PapirusText():
         font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', size)
 
         # Calculate the max number of char to fit on line
-        line_size = (papirus.width / (size*0.65))
+        line_size = (self.width / (size*0.65))
 
         current_line = 0
         text_lines = [""]
@@ -43,5 +47,5 @@ class PapirusText():
             current_line += 1
             draw.text( (0, ((size*current_line)-size)) , l, font=font, fill=BLACK)
 
-        self.papirus.display(image)
-        self.papirus.update()
+        self.display(image)
+        self.update()
