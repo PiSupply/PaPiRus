2bc6d677d8cb704678c4157456779d7e4e87af60
gibtmirdas <t.bertrand@outlook.com>
2017-03-22 23:02:06 +0000

    Add font path management

--- papirus/text.py
+++ papirus/text.py
@@ -13,7 +13,7 @@ class PapirusText():
     def __init__(self):
         self.papirus = Papirus()
 
-    def write(self, text, size = 20):
+    def write(self, text, size = 20, font_path='/usr/share/fonts/truetype/freefont/FreeMono.ttf'):
         
         # initially set all white background
         image = Image.new('1', self.papirus.size, WHITE)
@@ -21,7 +21,7 @@ class PapirusText():
         # prepare for drawing
         draw = ImageDraw.Draw(image)
 
-        font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', size)
+        font = ImageFont.truetype(font_path, size)
 
         # Calculate the max number of char to fit on line
         line_size = (self.papirus.width / (size*0.65))
