40a27224709ce9b79c4f8d886fa8f1dbf73cd0b0
Ton van Overbeek <tvoverbeek@gmail.com>
2017-10-26 20:34:03 +0200

    Python 3 fix for image.py
    Missed an integer division in image.py in the python 2 and 3 commit.
    See Issue #163.

--- papirus/image.py
+++ papirus/image.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import os
 import sys
 
@@ -20,8 +22,8 @@ class PapirusImage():
         if w > self.papirus.width or h > self.papirus.height:
             rsimg.thumbnail(self.papirus.size)
 
-        xpadding = (self.papirus.width  - rsimg.size[0]) / 2
-        ypadding = (self.papirus.height - rsimg.size[1]) / 2
+        xpadding = (self.papirus.width  - rsimg.size[0]) // 2
+        ypadding = (self.papirus.height - rsimg.size[1]) // 2
 
         image = Image.new('1', self.papirus.size, WHITE)
         image.paste(rsimg, (xpadding, ypadding))
