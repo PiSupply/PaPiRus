e8eb72dc97a438f9fb82bbd3af851069f8fe19dd
Aaron Shaw <shawaj@gmail.com>
2023-01-04 19:41:19 +0000

    Update image.py

--- papirus/image.py
+++ papirus/image.py
@@ -1,12 +1,10 @@
 from __future__ import division
 
-import os
-import sys
-
 from PIL import Image
 from papirus import Papirus
 
 WHITE = 1
+
 
 class PapirusImage(object):
 
@@ -16,13 +14,13 @@ class PapirusImage(object):
     def write(self, imagefile):
         fileimg = Image.open(imagefile)
 
-        w,h = fileimg.size
+        w, h = fileimg.size
 
         rsimg = fileimg
         if w > self.papirus.width or h > self.papirus.height:
             rsimg.thumbnail(self.papirus.size)
 
-        xpadding = (self.papirus.width  - rsimg.size[0]) // 2
+        xpadding = (self.papirus.width - rsimg.size[0]) // 2
         ypadding = (self.papirus.height - rsimg.size[1]) // 2
 
         image = Image.new('1', self.papirus.size, WHITE)
@@ -30,4 +28,3 @@ class PapirusImage(object):
 
         self.papirus.display(image)
         self.papirus.update()
-
