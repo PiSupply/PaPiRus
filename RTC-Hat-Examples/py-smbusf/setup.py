be408c44c09c07bb05a1368b450b286657e10a76
Aaron Shaw <shawaj@gmail.com>
2023-01-04 04:57:46 +0000

    Update setup.py

--- RTC-Hat-Examples/py-smbusf/setup.py
+++ RTC-Hat-Examples/py-smbusf/setup.py
@@ -2,13 +2,14 @@
 
 from distutils.core import setup, Extension
 
-setup(	name="smbusf",
-	version="1.1",
-	description="Python bindings for Linux SMBus access through i2c-dev, forced access version",
-	author="Mark M. Hoffman",
-	author_email="mhoffman@lightlink.com",
-	maintainer="Mark M. Hoffman",
-	maintainer_email="linux-i2c@vger.kernel.org",
-	license="GPLv2",
-	url="http://lm-sensors.org/",
-	ext_modules=[Extension("smbusf", ["smbusmodule.c"])])
+setup(
+    name="smbusf",
+    version="1.1",
+    description="Python bindings for Linux SMBus access through i2c-dev, forced access version",
+    author="Mark M. Hoffman",
+    author_email="mhoffman@lightlink.com",
+    maintainer="Mark M. Hoffman",
+    maintainer_email="linux-i2c@vger.kernel.org",
+    license="GPLv2",
+    url="http://lm-sensors.org/",
+    ext_modules=[Extension("smbusf", ["smbusmodule.c"])])
