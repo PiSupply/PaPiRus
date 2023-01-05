a1091361b29148f0e201a6f74a040a7434f32e2b
Aaron Shaw <shawaj@gmail.com>
2023-01-05 22:40:33 +0000

    Update text.py

--- papirus/text.py
+++ papirus/text.py
@@ -1,19 +1,17 @@
-import os
-import sys
-
 from PIL import Image, ImageDraw, ImageFont
 from papirus import Papirus
-import random
 
 WHITE = 1
 BLACK = 0
+
 
 class PapirusText(object):
 
     def __init__(self, rotation=0):
         self.papirus = Papirus(rotation=rotation)
 
-    def write(self, text, size=20, fontPath='/usr/share/fonts/truetype/freefont/FreeMono.ttf', maxLines=100):
+    def write(self, text, size=20,
+              fontPath='/usr/share/fonts/truetype/freefont/FreeMono.ttf', maxLines=100):
         
         # initially set all white background
         image = Image.new('1', self.papirus.size, WHITE)
@@ -35,7 +33,8 @@ class PapirusText(object):
             # Always add first word (even when it is too long)
             if len(textLines[currentLine]) == 0:
                 textLines[currentLine] += word
-            elif (draw.textsize(textLines[currentLine] + " " + word, font=font)[0]) < self.papirus.width:
+            elif (draw.textsize(textLines[currentLine] +
+                  " " + word, font=font)[0]) < self.papirus.width:
                 textLines[currentLine] += " " + word
             else:
                 # No space left on line so move to next one
@@ -45,10 +44,9 @@ class PapirusText(object):
                     textLines[currentLine] += word
 
         currentLine = 0
-        for l in textLines:
-            draw.text( (0, size*currentLine) , l, font=font, fill=BLACK)
+        for line in textLines:
+            draw.text( (0, size*currentLine) , line, font=font, fill=BLACK)
             currentLine += 1
 
         self.papirus.display(image)
         self.papirus.update()
-
